82a5b6f24d3c82725ad40742aebd5e90f96a9cb9
ThiaguitoUTU <160166098+ThiaguitoUTU@users.noreply.github.com>
2024-11-17 18:04:22 -0500

    Errores organizados

--- app/src/main/java/com/example/thirstyfriend/MainActivity3.kt
+++ app/src/main/java/com/example/thirstyfriend/MainActivity3.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class MainActivity3 : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
@@ -21,6 +23,12 @@ class MainActivity3 : AppCompatActivity() {
             insets
         }
 
+
+        val continue_button = findViewById<Button>(R.id.botonContinuar)
+        continue_button.setOnClickListener {
+            val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
+        }
 
     }
 
